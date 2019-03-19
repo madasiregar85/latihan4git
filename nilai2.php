74b648a66f0c4805d2ee24c3c037d849d3db07e7
madasiregar85 <48422794+madasiregar85@users.noreply.github.com>
2019-03-19 09:28:00 +0700

    Add files via upload

---
+++ nilai2.php
@@ -0,0 +1,27 @@
+
+<html>
+<head>
+	<title> "Nilai Ujian anda : $nilai<br>";</title>
+</head>
+<body>
+<font face=verdana size=2>
+<?php
+if(!empty($_POST['proses'])) { 
+	$nilai = $_POST['nilai'];
+echo "Nilai Ujian anda : $nilai<br>";
+if ($nilai<50)
+{
+echo "Anda Tidak Lulus<br>";	
+}
+elseif ($nilai<70)
+{
+echo "Anda Lulus Tapi Tidak Kompeten<br>";	
+} 
+else
+{
+echo "Selamat Anda Lulus dan Kompeten";
+}
+}
+?>
+</body>
+</html>
